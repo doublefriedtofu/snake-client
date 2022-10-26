c4b081159c5cd69d134eab3a8ad0286fd48b41dc
doublefriedtofu <hurdonghyuk@gmail.com>
2022-10-26 16:04:47 -0400

    added exit function

--- play.js
+++ play.js
@@ -1,3 +1,18 @@
 const { net, connect } = require("./client.js");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003' || key === "x") {
+    process.exit();
+  }
+}
+
 connect()
+setupInput()
